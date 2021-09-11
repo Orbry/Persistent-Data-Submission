1ac32af2c1c4ed4dc8153d7f00f10cd118e4c511
Orbry <orbryak@gmail.com>
2021-09-11 10:33:42 +0300

    moved managers to separate folder, added singleton class
    the singletong is kind of *soft* version, since there is no way to implement proper singleton derived from MonoBehaviour

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Due to the fact that MonoBehaviour prohibites using "new()" there seems to be no way
+ * to create proper Singleton abstract class that derives from MonoBehaviour. So what's
+ * left is to use *soft* Singleton that saves reference to first instance and warns about
+ * additional instances in console.
+ */
+abstract public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get => instance;
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            Debug.LogWarning($"An additional instance of \"{typeof(T).Name}\" was destroyed!");
+        } else
+            instance = (T)this;
+    }
+}
